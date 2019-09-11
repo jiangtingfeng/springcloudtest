b77c83bc2f6ebf9bf031bce0c29cc68410f04eb9
jiangtingfeng <2315507963@qq.com>
2019-09-11 21:32:01 +0800

    info:微服务实现

--- product-view-service-feign/src/main/java/org/test/springcloud/controller/ProductController.java
+++ product-view-service-feign/src/main/java/org/test/springcloud/controller/ProductController.java
@@ -1,6 +1,8 @@
 package org.test.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +12,19 @@ import org.test.springcloud.service.ProductService;
 import java.util.List;
 
 @Controller
+@RefreshScope
 public class ProductController {
 
     @Autowired
     ProductService productService;
 
+    @Value("${version}")
+    String version;
+
     @RequestMapping("/products")
     public Object products(Model m) {
         List<Product> ps = productService.listProducts();
+        m.addAttribute("version", version);
         m.addAttribute("ps", ps);
         return "products";
     }
