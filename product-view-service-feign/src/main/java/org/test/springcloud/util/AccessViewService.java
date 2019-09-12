71551ce12891bcaba9a065e6b958b800a71e40dd
jiangtingfeng <2315507963@qq.com>
2019-09-12 14:01:11 +0800

    info:配置监测数据服务（断路由监测）

---
+++ product-view-service-feign/src/main/java/org/test/springcloud/util/AccessViewService.java
@@ -0,0 +1,21 @@
+package org.test.springcloud.util;
+
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.http.HttpUtil;
+
+public class AccessViewService {
+
+    public static void main(String[] args) {
+
+        while(true) {
+            ThreadUtil.sleep(500);
+            try {
+                String html= HttpUtil.get("http://127.0.0.1:8012/products");
+                System.out.println("html length:" + html.length());
+            }
+            catch(Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+}
